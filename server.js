ec11c3c7d86b98a1fb053b9b7939c18f6a9958d5
HichamGsb <hichamgsb.pro@gmail.com>
2023-10-09 18:46:35 +0200

    Sever.js : Commentaires et code cleaning

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ const options = { // lecture clé privée et certificat
 
 // Gérer la méthode POST
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json({limit: '10mb'}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'CERISoNet/dist/ceriso-net')));
 
 // https://pedago.univ-avignon.fr:3201/
 const server = https.createServer(options, app);
@@ -40,59 +41,66 @@ app.use(session({
   cookie : {maxAge : 24 * 3600 * 1000} // millisecond valeur par défaut
 }));
 
-app.use(express.static(path.join(__dirname, 'CERISoNet/dist/ceriso-net')));
 
 // Accueil
 app.get('/', (req, res) => {
-  const indexPath = path.join(__dirname, 'CERISoNet/dist/ceriso-net', 'index.html');
+  const indexPath = path.join(__dirname, 'index.html');
   res.sendFile(indexPath);
 });
 
 // Connexion
-app.get('/login', (req, res) => {
+app.post('/login', (req, res) => {
   const identifiant = req.body.identifiant;
   const motdepasse = sha1(req.body.mot_de_passe);
-  console.log(identifiant + " " + motdepasse);
+
+  // Pour renvoyer les messages du serveur au niveau client.
+  const responseData = {};
+
   let sql = "select * from fredouil.users where identifiant='" + identifiant + "';";
-  
+
   const connexionObj = new pgClient.Pool({user:'uapv2102872', host:'127.0.0.1', database:'etd', password: 'jhFP6M', port:5432});
+
   connexionObj.connect((err, client, done) => {
     if(err) {
-      console.log('Erreur de connexion au serveur pg' + err.stack);
-      responseData.statusMsg = 'Erreur de connexion au serveur pg.';
+      console.log('Erreur de connexion au serveur pg.');
+      message = 'Erreur de connexion au serveur pg.';
     } else {
       console.log('Connexion établie / pg db server');
+      message = 'Connexion établie / pg db server';
 
-      const responseData = {};
-
+      // Query send to BDD PGSQL
       client.query(sql, (err, result) => {
         if(err) {
           console.log('Erreur d execution de la requete' + err.stack);
-          responseData.statusMsg = 'Connexion échouée';
+          message += 'Erreur d execution de la requete';
+          res.send({ message: message })
         } 
         else if ((result.rows[0] != null) && (result.rows[0].motpasse == motdepasse)) {
           req.session.isConnected = true;
           req.session.identifiant = identifiant;
 
-          responseData.data = result.rows[0].nom;
-          responseData.statusMsg = 'Connexion réussie : Bonjour '+ result.rows[0].prenom;
+          message = 'Connexion réussie : Bonjour '+ result.rows[0].prenom;
 
           // Mettre à jour le statut de connexion dans la base de données
           const updateSql = `UPDATE fredouil.users SET statut_connexion = 1 WHERE identifiant = '${identifiant}';`;
-          
+
+          // UPDATE query
           client.query(updateSql, (err, updateResult) => {
             if (err) {
               console.log('Erreur lors de la mise à jour du statut de connexion :', err.stack);
-              responseData.statusMsg += 'Erreur lors de la mise à jour du statut de connexion';
+              message += 'Erreur lors de la mise à jour du statut de connexion';
+              res.status(401).send({ message: message });
             } else {
               console.log('Statut de connexion mis à jour dans la base de données.');
-              responseData.statusMsg += 'Statut de connexion mis à jour dans la base de données.';
+              message += 'Statut de connexion mis à jour dans la base de données.';
+              res.send({ message: message });
             }
           });
         }
         else {
           console.log('Connexion échouée : informations de connexion incorrectes.');
-          responseData.statusMsg='Connexion échouée : informations de connexion incorrectes.';
+          message = 'Connexion échouée : informations de connexion incorrectes.';
+          res.status(401).send({ message: message });
         }
       })
     }
@@ -124,16 +132,19 @@ app.get('/logout', (req, res) => {
       }
       client.release();
     });
+
     // Déconnexion de l'utilisateur en réinitialisant la session
     req.session.destroy(err => {
       if (err) {
         console.log('Erreur lors de la déconnexion :', err);
       } else {
         console.log('Utilisateur déconnecté avec succès.');
+        res.send({ message : "Vous avez été déconnecté."});
       }
     });
   } else {
     // L'utilisateur n'était pas connecté
-    res.redirect('/');
+    console.log("L'utilisateur n'était pas connecté.")
+    res.send({ message : "Erreur : Vous n'étiez pas connecté."});
   }
 });
