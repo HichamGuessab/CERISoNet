8d77c933754e3a75ccf6c7d21767712a7d24845c
HichamGsb <hichamgsb.pro@gmail.com>
2023-10-13 02:13:36 +0200

    Front-end : Affichage des messages et des commentaires

--- CERISoNet/src/app/message/message.component.ts
+++ CERISoNet/src/app/message/message.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html'
 })
-export class MessageComponent {
+export class MessageComponent implements OnInit{
+  messages: any[] = [];
 
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit() {
+    this.messageService.getMessages().subscribe((messages) => {
+      this.messages = messages;
+    })
+  }
 }
