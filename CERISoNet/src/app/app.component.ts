473ebfd94c538ff325cb911cead0a734c36ebf95
HichamGuessab <hichamgsb.pro@gmail.com>
2023-12-16 12:38:33 +0100

    Guard authentification

--- CERISoNet/src/app/app.component.ts
+++ CERISoNet/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthentificationService} from "./authentification.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CERISoNet';
+
+  isConnected: boolean;
+
+  constructor(private authentificationService: AuthentificationService) {}
+
+  ngOnInit() {
+    this.authentificationService.getIsConnectedObservable().subscribe( isConnected => {
+      this.isConnected = isConnected;
+    })
+  }
+
+
 }
